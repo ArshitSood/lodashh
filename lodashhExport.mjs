61de0cacee9a3cc7a6d9a5751dbfd6298e9bcee3
Arshit Sood <asood2_be22@thapar.edu>
2024-07-04 15:26:52 +0530

    added more array methods to SimpleLodash class

--- lodashhExport.mjs
+++ lodashhExport.mjs
@@ -1,22 +1,25 @@
 export class SimpleLodash {
-    static chunk(array, size) {
+    chunk(array, size) { //It is used to break the array in to small chunks.
         const result = [];
         for (let i = 0; i < array.length; i += size) {
             result.push(array.slice(i, i + size));
         }
         return result
     }
-    static compact(array) {
+
+    compact(array) { //It is used to creates an array with all falsey values removed
         return array.filter(Boolean);
     }
-    static concat(...arrays) {
+
+    concat(...arrays) { //It is used to concatenating the arrays
         let result = [];
         for (let array of arrays) {
             result = result.concat(array)
         }
         return result;
     }
-    static difference(array, values) {
+
+    difference(array, values) { //It is used to remove a single element or the array of elements from the original array.
         const result = [];
         for (let item of array) {
             if (!values.includes(item)) {
@@ -25,24 +28,132 @@ export class SimpleLodash {
         }
         return result;
     }
-    static drop(array, n = 1) {
+
+    drop(array, n = 1) { //It is used to drop the elements in a given array.
         return array.slice(n);
     }
 
-    static dropRight(array, n = 1) {
+    dropRight(array, n = 1) { //It is used to delete the elements from the right of the array.
         return array.slice(0, -n);
     }
-    static fill(array, value, start = 0, end = array.length) {
+
+    fill(array, value, start = 0, end = array.length) { //It is used to fill a set of values into the array in a given range
         if (start < 0) {
             start = Math.max(array.length + start, 0);
         }
         if (end < 0) {
             end = Math.max(array.length + end, 0);
         }
-        for (let i = start; i < end; i++) {
+        for (let i = start; i <= end; i++) {
             array[i] = value;
         }
         return array
     }
 
+    findIndex(array, num) { //It is used to find the index of the first occurrence of the element.
+        return array.indexOf(num);
+    }
+
+    findLastIndex(array, num) { //It is used to find the element from the right of the array.
+        return array.lastIndexOf(num);
+    }
+    flattenDepth(array, depth = 1) { //It is used to flatten up to depth time that is passed into the function.
+        return array.flat(depth);
+    }
+
+    flattenDeep(array) { //It is used to completely flatten nested arrays.
+        return array.flat(Infinity);
+    }
+
+    fromPairs(pairs) { //It returns an object composed form key-value pairs.
+        const result = {};
+        pairs.forEach(([key, value]) => {
+            result[key] = value;
+        });
+        return result;
+    }
+
+    head(array) { //It is used to get the first element of an array.
+        return array[0];
+    }
+
+    intersection(array1, array2) { //It is used to take the intersection of the one or more arrays.
+        return array1.filter(item => array2.includes(item));
+    }
+
+    join(array, separator = ',') { //It is used to converts all elements in the array into a string separated by a separator.
+        return array.join(separator);
+    }
+
+    pull(array, ...values) { //It is used to remove all the given values from a given array.
+        return array.filter(item => !values.includes(item));
+    }
+
+    pullAll(array, values) { //2nd argument is an array
+        //It is used to remove all the values from the first given array that are given in the second array.
+        return array.filter(item => !values.includes(item));
+    }
+
+    pullAt(array, ...indexes) { //It removes the element corresponding to the given address.
+        indexes.sort((a, b) => b - a);
+        const pulled = [];
+        for (let index of indexes) {
+            pulled.push(array.splice(index, 1)[0]);
+        }
+        return pulled.reverse();
+    }
+
+    remove(array, value) { //it removes all elements from the array that predicate returns True.
+        return array.filter(item => !value(item));
+    }
+
+    reverse(array) {
+        return array.reverse();
+    }
+
+    take(array, n = 1) { //It creates a slice of an array with n elements from the beginning.
+        if (n < 0) return [];
+        return array.slice(0, n);
+    }
+
+    takeRight(array, n = 1) { //It is used to get the array of n elements from the end of the given array
+        const length = array.length;
+        if (n < 0) return [];
+        return array.slice(Math.max(length - n, 0));
+    }
+
+    union(...arrays) { //It takes n number of arrays and creates an array of unique values in order.
+        const result = [];
+        arrays.forEach(arr => {
+            arr.forEach(item => {
+                if (!result.includes(item)) {
+                    result.push(item);
+                }
+            });
+        });
+        return result;
+    }
+
+    uniq(array) { //It creates an array of unique values in order.
+        const result = [];
+        array.forEach(item => {
+            if (!result.includes(item)) {
+                result.push(item);
+            }
+        });
+        return result;
+    }
+
+    xor(array1, array2) {
+        // a XOR b = (a OR b) - (a AND b)
+        const a = this.union(array1, array2);
+        const b = this.intersection(array1, array2);
+        return Array.from(a).filter(item => !b.includes(item));
+    }
+
+    without(array,...values){ //same as pull
+        //creates a new array in a filtered form that is there are values to exclude and give new values as output.
+        return array.filter(item => !values.includes(item));
+    }
 }
+
--- lodashhImport.mjs
+++ lodashhImport.mjs
@@ -1,18 +1,76 @@
 import { SimpleLodash } from "./lodashhExport.mjs";
+
+const _ = new SimpleLodash();
+
 let array = [1, 2, 3, 4, 5, 6, 7];
-console.log(SimpleLodash.chunk(array, 2));
 
-array = [0, 1, 2, 3, 4, false, undefined, NaN, 5, 6, 7];
-console.log(SimpleLodash.compact(array));
-
-const array1 = [1, 2, 3, 4];
-const array2 = [2, 3];
-const array3 = [4, 5, 6];
-let y = SimpleLodash.concat(array1, array2, array3)
+let array1 = [1, 2, 3, 4];
+let array2 = [2, 3];
+let array3 = [4, 5, 6];
+//array = [0, 1, 2, 3, 4, false, undefined, NaN, 5, 6, 7];
+console.log(_.compact(array));
+console.log(_.chunk(array, 2));
+let y = _.concat(array1, array2, array3);
 console.log(y);
-console.log(SimpleLodash.difference(array1, array2));
-console.log(SimpleLodash.drop(array1, 2));
-console.log(SimpleLodash.dropRight(array1, 2));
+console.log(_.difference(array1, array2));
+console.log(_.drop(array1, 2));
+console.log(_.dropRight(array1, 2));
 
 console.log(y);
-console.log(SimpleLodash.fill(y, '%', 1, 4));
+console.log(_.fill(y, 'xX*Xx', 1, 3));
+
+console.log(_.findIndex(y, 2));
+console.log(_.findLastIndex(y, 2));
+
+const nestedArray = [1, [2, [3, [4]], 5]];
+const flatArray = _.flattenDepth(nestedArray, 2);
+console.log(flatArray);
+const flatArray2 = _.flattenDeep(nestedArray);
+console.log(flatArray2);
+
+const pairs = [['a', 1], ['b', 2], ['c', 3]];
+const object = _.fromPairs(pairs);
+console.log(object);
+
+
+console.log(_.head(array)); 
+
+console.log(_.intersection(array1, array2));
+
+console.log(_.join(array, '-'));
+
+console.log(_.pull(array, 3, 5));
+
+const valuesToRemove = [2, 4];
+console.log(_.pullAll(array, valuesToRemove));
+
+console.log(_.pullAt(array, 1, 3)); 
+
+console.log(_.remove(array, n => n % 2 === 0)); 
+
+console.log(_.reverse(array));
+
+console.log(_.take(array, 3))
+
+
+console.log(_.takeRight(array, 3))
+
+
+array1 = [1, 2, 3];
+array2 = [2, 3, 4];
+console.log(_.union(array1, array2));
+
+
+array3 = [1, 2, 2, 3, 3, 3];
+console.log(_.uniq(array3))
+
+
+let array4 = [1, 2, 3];
+let array5 = [2, 3, 4];
+console.log(_.xor(array4, array5));
+
+let w = _.without([3, 5, 5, 1], 1, 3);
+let x = _.pull([3, 5, 5, 1], 1, 3);
+let t = _.without([4, 7, 4, 8], 7, 4);
+console.log(w);
+console.log(t);
